aa68f72ee3e85e3685199eae00fe5714dd258962
HugoCaparros <hcaparros@campuscamarafp.com>
2026-02-03 11:38:21 +0100

    Add files via upload

---
+++ backend/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db = getenv("MYSQLDATABASE");
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+if ($conn->connect_error){
+    die ("Error de conexión: ".$conn->connect_error);
+}
+
+?>
